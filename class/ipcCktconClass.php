22fb27474c8ec3ed430feb22333d8f3a3b244862
Kris Pham <kristopher.pham@gmail.com>
2019-05-15 18:06:48 -0500

    cleaned up sql

--- class/ipcCktconClass.php
+++ class/ipcCktconClass.php
@@ -205,7 +205,9 @@ class CKTCON {
             return;
         }
         $con = $result["cktcon"];
-        $qry = "INSERT INTO t_cktcon VALUES (0, '$con', '$ckt_id', '$ckid', 1, '$ctyp', '$ctyp', '$fp_id', '$fport', $fp_n, '$tp_id', '$tport', $tp_n, $path)";
+        // $qry = "INSERT INTO t_cktcon VALUES (0, '$con', '$ckt_id', '$ckid', 1, '$ctyp', '$ctyp', '$fp_id', '$fport', $fp_n, '$tp_id', '$tport', $tp_n, $path)";
+        $qry = "INSERT INTO t_cktcon (con, ckt_id, ckid, idx, ctyp, ctyp_o, fp_id, fport, fp_n, tp_id, tport, tp_n, path) ";
+        $qry = " VALUES('$con', '$ckt_id', '$ckid', 1, '$ctyp', '$ctyp', '$fp_id', '$fport', '$fp_n', $tp_id', '$tport', '$tp_n', '$path')";
 		$res = $db->query($qry);
         if (!$res) {
             $this->rslt = FAIL;
@@ -244,7 +246,9 @@ class CKTCON {
         $idx = $result['idx'];
 
         //$qry = "INSERT INTO t_cktcon VALUES(0,'$con','$ckt_id','$idx','$ctyp','$ctyp_o','$fp_id','$fp_n','$tp_id','$tp_n')";
-        $qry = "INSERT INTO t_cktcon VALUES(0, '$con', '$ckt_id', '$ckid', $idx, '$ctyp', '$ctyp_o', '$fp_id', '$fport', $fp_n, '$tp_id', '$tport', $tp_n, $path)";
+        // $qry = "INSERT INTO t_cktcon VALUES(0, '$con', '$ckt_id', '$ckid', $idx, '$ctyp', '$ctyp_o', '$fp_id', '$fport', $fp_n, '$tp_id', '$tport', $tp_n, $path)";
+        $qry = "INSERT INTO t_cktcon (con, ckt_id, ckid, idx, ctyp, ctyp_o, fp_id, fport, fp_n, tp_id, tport, tp_n, path) ";
+        $qry .= " VALUES ('$con', $ckt_id, $ckid, '$idx', '$ctyp', '$ctyp_o', '$fp_id', '$fport', '$fp_n', '$tp_id', '$tport', '$tp_n', '$path')";
 
         $res = $db->query($qry);
         if (!$res) {
