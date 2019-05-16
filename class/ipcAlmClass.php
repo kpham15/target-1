366a99f0c51e3dde3e25f231f0f4ff48933619b1
Kris Pham <kristopher.pham@gmail.com>
2019-05-16 10:23:00 -0500

    restructure newAlm

--- class/ipcAlmClass.php
+++ class/ipcAlmClass.php
@@ -136,8 +136,13 @@ class ALMS {
 
         // $qry = "INSERT INTO t_alms VALUES(0, '$almid', '', '$sa', '$src', '$type', '$cond', '$sev', '$psta', '$ssta', '$remark', '$now')";
 
-        $qry = "INSERT INTO t_alms (almid, sa, src, type, cond, sev, psta, ssta, remark, datetime) ";
-        $qry .= " VALUES ('$almid', '$sa', '$src', '$type', '$cond', '$sev', '$psta', '$ssta', '$remark', '$now')";
+        $qry = "INSERT INTO 
+                t_alms 
+                (almid, sa, src, type, cond, 
+                sev, psta, ssta, remark, datetime) 
+                VALUES 
+                ('$almid', '$sa', '$src', '$type', '$cond', 
+                '$sev', '$psta', '$ssta', '$remark', '$now')";
 
         $res = $db->query($qry);
 		if (!$res) {
