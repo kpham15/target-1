842ada044ad919d69c60536e7a84fecc3e4e66af
Kris Pham <kristopher.pham@gmail.com>
2019-05-16 17:18:20 -0500

    add dot

--- class/ipcDbClass.php
+++ class/ipcDbClass.php
@@ -17,7 +17,7 @@ class DB {
         // obtain db info
         // $file = __DIR__ . "/ipc-db.cfg";
         //@TODO below is the directory 
-        $file = __DIR__ . "/../../ipc-db.cfg";
+        $file = __DIR__ . "./../../ipc-db.cfg";
         $dbString = file_get_contents($file);
         $parseDbString = explode(",", $dbString);
         $dbHost = $parseDbString[0];
