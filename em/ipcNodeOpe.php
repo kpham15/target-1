912d77445c925ab387a634b43e2937fae855b4aa
Kris Pham <kristopher.pham@gmail.com>
2019-05-22 16:29:33 -0500

    added queryAll function

--- em/ipcNodeOpe.php
+++ em/ipcNodeOpe.php
@@ -16,21 +16,24 @@ if (isset($_POST['rack']))
 $serial_no = "";
 if (isset($_POST['serial_no']))
     $serial_no = $_POST['serial_no'];
-    
+
+$device = "";
+if (isset($_POST['device']))
+    $device = $_POST['device'];
+        
 
 // dispatch to functions
 
 
 if ($act == "queryAll") {
-	
-	
+	$result = queryAll();	
 	echo json_encode($result);
 	mysqli_close($db);
 	return;
 }
 
 if ($act == "DISCOVER") {
-
+    $result = discover($node, $device);
     echo json_encode($result);
 	mysqli_close($db);
 	return;
@@ -38,27 +41,41 @@ if ($act == "DISCOVER") {
 
 
 if ($act == "START") {
-
+    $result = start($node, $device);
     echo json_encode($result);
 	mysqli_close($db);
 	return;
 }
 
 if ($act == "STOP") {
-
+    $result = stop($node, $device);
     echo json_encode($result);
 	mysqli_close($db);
 	return;
 }
 
 if ($act == "UPDATE RACK") {
-
+    $result = updateRack($node, $device)
     echo json_encode($result);
 	mysqli_close($db);
 	return;
 }
 
 // functions area
+
+function queryAll() {
+    $cpssObj = new CPSS();
+    if ($cpssObj->rslt == FAIL) {
+        $result['rslt'] = $cpssObj->rslt;
+        $result['reason'] = $cpssObj->reason;
+        return $result;
+    }
+
+    $result['rslt'] = SUCCESS;
+    $result['reason'] = "QUERY SUCCESS";
+    $result['rows'] = $cpssObj->rows;
+    return $result;
+}
 
 
 ?>
