9c2022a22e2a5a19e9bdeea84ec104e7df644e70
Kris Pham <kristopher.pham@gmail.com>
2019-05-22 16:16:04 -0500

    added dispatch area

--- em/ipcNodeOpe.php
+++ em/ipcNodeOpe.php
@@ -18,4 +18,47 @@ if (isset($_POST['serial_no']))
     $serial_no = $_POST['serial_no'];
     
 
+// dispatch to functions
+
+
+if ($act == "queryAll") {
+	
+	
+	echo json_encode($result);
+	mysqli_close($db);
+	return;
+}
+
+if ($act == "DISCOVER") {
+
+    echo json_encode($result);
+	mysqli_close($db);
+	return;
+}
+
+
+if ($act == "START") {
+
+    echo json_encode($result);
+	mysqli_close($db);
+	return;
+}
+
+if ($act == "STOP") {
+
+    echo json_encode($result);
+	mysqli_close($db);
+	return;
+}
+
+if ($act == "UPDATE RACK") {
+
+    echo json_encode($result);
+	mysqli_close($db);
+	return;
+}
+
+// functions area
+
+
 ?>
