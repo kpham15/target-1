f093a3f84b7dbad93f9bdeb9d9fe7f11fce7621e
Bien Nguyen <bien.g.nguyen@gmail.com>
2019-05-23 15:44:05 -0500

    debug sidebar

--- index.php
+++ index.php
@@ -85,13 +85,22 @@
 	// include './html/ipcMainEnd.html';
 	
 	
-	include './js/dependencies.php';
+	// include './js/dependencies.php';
 	include './js/functions.php';
 	
 	echo '</body>';
 	echo '</html>';
 	
 ?>
+
+<script src="./js/ipcOpt.js"></script>
+<script src="./js/hmac-sha256.js"></script>
+<script src="./js/enc-base64-min.js"></script>
+
+<script src="./bower_components/jquery/dist/jquery.min.js"></script>
+<script src="./bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="./plugins/iCheck/icheck.min.js"></script>
+<script src="./dist/js/adminlte.min.js"></script>
 
 <script>
 
