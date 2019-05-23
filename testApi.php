bb399c2e99540fb6d166c0bda8dafdd9c3a03c58
Alexander Nguyen-Huynh <AlexNHPQ@ALEXANDERs-MacBook-Pro.local>
2019-05-23 16:56:22 -0500

    testApi

---
+++ testApi.php
@@ -0,0 +1,33 @@
+<?php
+
+function dev($device_status) {
+
+    // the URL to post to which API (Its almost always going to be Dispatch)
+    // Array that would POST to the API as if it were from front end GUI
+    // 'act' is the dispatch function being called
+    $url = "http://localhost/workspaces/alex/em/ipcDispatch.php";
+    $data = array('api' => "ipcNodeOpe", 'user'=>'system', 'act' => 'CPS_STATUS', 'cmd' => $cmd);
+
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+    $context  = stream_context_create($options);
+    $response = json_decode(file_get_contents($url, false, $context));
+    return $response;
+}
+
+
+$resp = dev('\$ackid=1-CPS,status,current=1239mA,voltage=45678mV*');
+// echo $resp->rslt . ": " . $resp->reason . "\n";
+$cmd = "$ackid=1-cps,status,voltage1=46587mV,voltage2=47982mV,voltage3=48765mV,voltage4=49234mV*";
+// print_r($resp->rslt);
+print_r($resp->reason);
+// $resp = almApi('\$ackid=0-cps,status,temperature,zone1=67C,zone2=65C,zone3=66C,zone4=68C*');
+// echo $resp->rslt . ": " . $resp->reason . "\n";
+
+
+?>
