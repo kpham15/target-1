511d7adb6ff55fb2c497f9b1bb8844946e28a65f
Bien Nguyen <bien.g.nguyen@gmail.com>
2019-05-24 13:24:47 -0500

    debug

--- pages/nav-wrapper/nav-wrapper.php
+++ pages/nav-wrapper/nav-wrapper.php
@@ -1,16 +1,16 @@
 <div id="nav-wrapper" class="wrapper" style="display: none;">
 
   <!-- Main Header -->
-  <?php //include __DIR__ . '/header-navbar.html'; ?>
+  <?php require __DIR__ . '/header-navbar.html'; ?>
 
   <!-- Left side column sidebar -->
-  <?php //include __DIR__ . '/sidebar-nav.html'; ?>
+  <?php require __DIR__ . '/sidebar-nav.html'; ?>
 
   <!-- Content Wrapper. Contains page content -->
   <div id="main-section-content" class="content-wrapper">
     
     <!-- Includes for all the content pages -->
-    <?php include __DIR__ . '../system-view.php'; ?>
+    <?php require __DIR__ . '../system-view.php'; ?>
 
     <!-- /.content -->
   </div>
