8ad39cd92be0b5bbbc09a2ad31df3483157a1bef
Bien Nguyen <bien.g.nguyen@gmail.com>
2019-05-24 13:51:16 -0500

    testing

--- js/dependencies.php
+++ js/dependencies.php
@@ -11,4 +11,11 @@
   echo '<link rel="stylesheet" href="./css/ipcv2.css">';
 
   echo '<link rel="stylesheet" href="./plugins/iCheck/square/blue.css">';
+
+  echo '<script src="bower_components/jquery/dist/jquery.min.js"></script>';
+  echo '<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>';
+  echo '<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>';
+  echo '<script src="bower_components/fastclick/lib/fastclick.js"></script>';
+  echo '<script src="dist/js/adminlte.min.js"></script>';
+  echo '<script src="plugins/iCheck/icheck.min.js"></script>';
 ?>
--- pages/nav-wrapper/nav-wrapper.php
+++ pages/nav-wrapper/nav-wrapper.php
@@ -23,14 +23,3 @@
   <?php //include __DIR__ . "/control-sidebar.html"; ?>
 </div>
 <!-- ./wrapper -->
-
-<!-- jQuery 3 -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- Slimscroll -->
-<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
