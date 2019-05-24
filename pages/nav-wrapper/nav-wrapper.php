d63f4924b9ca57b626eff70a9ea5c27e3a876580
Bien Nguyen <bien.g.nguyen@gmail.com>
2019-05-24 13:13:29 -0500

    compartmentalization of components

--- pages/nav-wrapper/nav-wrapper.php
+++ pages/nav-wrapper/nav-wrapper.php
@@ -1,76 +1,16 @@
 <div id="nav-wrapper" class="wrapper" style="display: none;">
 
   <!-- Main Header -->
-  <header class="main-header">
+  <?php include 'header-navbar.html'; ?>
 
-    <!-- Logo -->
-    <a href="index2.html" class="logo">
-      <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><img src="./resources/Telepath_Logo_Part.JPG" height="50" width="50"/></span>
-      <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Telepath</b>Networks&nbsp;Inc.</span>
-    </a>
-
-    <?php include 'header-navbar.html'; ?>
-  </header>
-  <!-- Left side column. contains the logo and sidebar -->
-  <aside class="main-sidebar">
-
-    <!-- sidebar: style can be found in sidebar.less -->
-    <section class="sidebar">
-
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel">
-        <div class="pull-left image">
-          <img src="./dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-        </div>
-        <div class="pull-left info">
-          <p id="sidebar-user-name">Alexander Pierce</p>
-          <!-- Status -->
-          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-        </div>
-      </div>
-
-      <!-- search form (Optional) -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-          <span class="input-group-btn">
-              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-              </button>
-            </span>
-        </div>
-      </form>
-      <!-- /.search form -->
-
-      <!-- Sidebar Menu -->
-      <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">Navigation</li>
-        <!-- Optionally, you can add icons to the links -->
-        <li class="active"><a href="#"><i class="glyphicon glyphicon-th"></i> <span>System View</span></a></li>
-        <!-- <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
-        <li class="treeview">
-          <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
-            <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-              </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#">Link in level 2</a></li>
-            <li><a href="#">Link in level 2</a></li>
-          </ul>
-        </li> -->
-      </ul>
-      <!-- /.sidebar-menu -->
-    </section>
-    <!-- /.sidebar -->
-  </aside>
+  <!-- Left side column sidebar -->
+  <?php include 'sidebar-nav.html'; ?>
 
   <!-- Content Wrapper. Contains page content -->
   <div id="main-section-content" class="content-wrapper">
     
     <!-- Includes for all the content pages -->
-    <?php include 'system-view.php'; ?>
+    <?php include '../system-view.php'; ?>
 
     <!-- /.content -->
   </div>
