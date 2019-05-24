e81537bc3311b48ad3e3c83ac2988de2f1da355c
Bien Nguyen <bien.g.nguyen@gmail.com>
2019-05-24 15:49:13 -0500

    add color to node status depending on alarm

--- js/dependencies.php
+++ js/dependencies.php
@@ -8,9 +8,9 @@
   echo '<link rel="stylesheet" href="./dist/css/skins/skin-blue.min.css">';
   echo '<link rel="stylesheet" href="./dist/css/fonts.css">';
 
-  echo '<link rel="stylesheet" href="./css/ipcv2.css">';
-
   echo '<link rel="stylesheet" href="./plugins/iCheck/square/blue.css">';
+  
+  echo '<link rel="stylesheet" href="./css/ipcv2.css">';
 
   echo '<script src="bower_components/jquery/dist/jquery.min.js"></script>';
   echo '<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>';
