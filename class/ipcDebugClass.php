6095b7fcb1cb827d060504ca4f6dada3edca8563
bhdninh1 <you@example.com>
2019-05-28 18:31:33 -0500

    Added fild discriptor $debugFile, DEBUG->log, DEBUG->close

--- class/ipcDebugClass.php
+++ class/ipcDebugClass.php
@@ -2,6 +2,7 @@
 
 class DEBUG{
     public $enable = 0;
+    public $debugFile = null;
     public $rslt;
     public $reason;
 
@@ -13,62 +14,50 @@ class DEBUG{
             return;
         }
         $enable = 0;
-        $debugFile = fopen("../../ipc-debug.cfg", "r");
+        $fd = fopen("../../ipc-debug.cfg", "r");
         if (!$debugFile) {
             $this->rslt = 'fail';
             $this->reason = "OPEN_LOGFILE_FAIL";
             return;
         }
         else {
-            while (($line = fgets($file)) !== false) {
+            while (($line = fgets($fd)) !== false) {
                 // process the line read.
                 $enable = trim($line);
-                if($enable != '') break;
+                if ($enable != '')
+                    break;
             }
         
-            fclose($file);
+            fclose($fd);
         }
+
         $this->enable = $enable;
+
+        if ($this->enable == 1) {
+            $this->debugFile =  fopen("../report/debug.log", "a");
+        }
+        
         $this->rslt = 'success';
         $this->reason = "DEBUG CONSTRUCTED";
 
     }
 
-    public function logPostRequestInfo($filename,$variableArray) {
-        $this->logString = "-----------------------\n".date("Y-m-d H:i:s")." ".$filename.": ";
-        $lastkey = end(array_keys($variableArray));
-        foreach($variableArray as $key => $value) {
-            if ($key ===  $lastkey) {
-                $this->logString .= $key.":".$value."\n";
-            }
-            else {
-                $this->logString .= $key.":".$value.", ";
-            }
-                
+    public function close() {
+        if ($this->enable == 0)
+            return;
+        fclose($this->debugFile);
+    }
+
+    public function log($string) {
+        if ($this->enable == 0) {
+            return;
         }
+
+        fwrite($this->debugFile, $string);
+
     }
 
-    public function logFunction($funcName) {
-        $this->logString .= $funcName."()\n";
-    }
     
-    public function logEvent($result, $reason) {
-        $this->logString .= $result.": ".$reason."\n";
-
-        // $response = fwrite($this->debugFile,$this->logString);
-        // if(!$response) {
-        //     $this->rslt = 'fail';
-        //     $this->reason = "WRITE_LOGFILE_FAIL";
-        //     return;
-        // }
-        // will insert log to DB instead
-        $this->rslt = 'success';
-        $this->reason = "WRITE_LOGFILE_SUCCESS";
-    }
-
-    public function closeLogfile(){
-        // fclose($this->debugFile);
-    }
 
 }
 
