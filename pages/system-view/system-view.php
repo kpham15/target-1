d33feb0c441ca60ac24897082656b58c02d7fec7
Bien Nguyen <bien.g.nguyen@gmail.com>
2019-05-28 16:52:19 -0500

    forgot return

--- pages/system-view/system-view.php
+++ pages/system-view/system-view.php
@@ -126,7 +126,9 @@
 
   function createMioBtn(index, ptyp) {
     let slot = index + 1;
-    let mioBtn = '<button type="button" class="mio-btn btn btn-default" slot="'+slot+'" ptyp="'+ptyp+'">MIO'+ptyp.toUpperCase()+'-'+slot+'</button>'
+    let mioBtn = '<button type="button" class="mio-btn btn btn-default" slot="'+slot+'" ptyp="'+ptyp+'">MIO'+ptyp.toUpperCase()+'-'+slot+'</button>';
+
+    return mioBtn;
   }
 
   function createNodeTabs(node) {
