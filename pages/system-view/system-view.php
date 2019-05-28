8b1ddef5c8086649e41d979f3d096eb2fed17a74
Bien Nguyen <bien.g.nguyen@gmail.com>
2019-05-28 15:58:09 -0500

    dynamically generate port boxes

--- pages/system-view/system-view.php
+++ pages/system-view/system-view.php
@@ -25,7 +25,7 @@
           <ul id="node-x-tabs" class="nav nav-tabs node-tabs">
             <!-- Node tabs for X side created dynamically -->
           </ul>
-          <div class="tab-content mio-tabs">
+          <div id="mio-x-table" class="tab-content mio-tabs">
             <div class="tab-pane active" id="nodex1">
               <div class="container-fluid">
                 <div class="row">
@@ -48,362 +48,10 @@
                     <button type="button" class="btn btn-default">26-50</button>
                   </div>
                 </div>
-                <div class="row port-grid">
+                <div id="x-port-grid" class="row port-grid">
 
-                  <div class="info-box bg-yellow">
-                    <div class="info-box-text">
-                      1-1-X-1
-                      <span class="pull-right">MTCD</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-001
-                      <span class="pull-right">OE</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      512-222-2222
-                    </div>
-                  </div>
-                  <div class="info-box bg-aqua">
-                    <div class="info-box-text">
-                      1-1-X-2
-                      <span class="pull-right">UAS</span>
-                    </div>
-                    <div class="info-box-text">
-                      -
-                      <span class="pull-right">-</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-aqua">
-                    <div class="info-box-text">
-                      1-1-X-3
-                      <span class="pull-right">UAS</span>
-                    </div>
-                    <div class="info-box-text">
-                      -
-                      <span class="pull-right">-</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-aqua">
-                    <div class="info-box-text">
-                      1-1-X-4
-                      <span class="pull-right">UAS</span>
-                    </div>
-                    <div class="info-box-text">
-                      -
-                      <span class="pull-right">-</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-aqua">
-                    <div class="info-box-text">
-                      1-1-X-5
-                      <span class="pull-right">UAS</span>
-                    </div>
-                    <div class="info-box-text">
-                      -
-                      <span class="pull-right">-</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-green">
-                    <div class="info-box-text">
-                      1-1-X-6
-                      <span class="pull-right">CONN</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-006
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      555-555-5555
-                    </div>
-                  </div>
-                  <div class="info-box bg-green">
-                    <div class="info-box-text">
-                      1-1-X-7
-                      <span class="pull-right">CONN</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-007
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      555-555-5555
-                    </div>
-                  </div>
-                  <div class="info-box bg-aqua">
-                    <div class="info-box-text">
-                      1-1-X-8
-                      <span class="pull-right">UAS</span>
-                    </div>
-                    <div class="info-box-text">
-                      -
-                      <span class="pull-right">-</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-aqua">
-                    <div class="info-box-text">
-                      1-1-X-9
-                      <span class="pull-right">UAS</span>
-                    </div>
-                    <div class="info-box-text">
-                      -
-                      <span class="pull-right">-</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-aqua">
-                    <div class="info-box-text">
-                      1-1-X-10
-                      <span class="pull-right">UAS</span>
-                    </div>
-                    <div class="info-box-text">
-                      -
-                      <span class="pull-right">-</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-red">
-                    <div class="info-box-text">
-                      1-1-X-11
-                      <span class="pull-right">SF</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-011
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text=center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-red">
-                    <div class="info-box-text">
-                      1-1-X-12
-                      <span class="pull-right">SF</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-012
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-aqua">
-                    <div class="info-box-text">
-                      1-1-X-13
-                      <span class="pull-right">UAS</span>
-                    </div>
-                    <div class="info-box-text">
-                      -
-                      <span class="pull-right">-</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-aqua">
-                    <div class="info-box-text">
-                      1-1-X-14
-                      <span class="pull-right">UAS</span>
-                    </div>
-                    <div class="info-box-text">
-                      -
-                      <span class="pull-right">-</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-aqua">
-                    <div class="info-box-text">
-                      1-1-X-15
-                      <span class="pull-right">UAS</span>
-                    </div>
-                    <div class="info-box-text">
-                      -
-                      <span class="pull-right">-</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-red">
-                    <div class="info-box-text">
-                      1-1-X-16
-                      <span class="pull-right">SF</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-016
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-red">
-                    <div class="info-box-text">
-                      1-1-X-17
-                      <span class="pull-right">SF</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-017
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-red">
-                    <div class="info-box-text">
-                      1-1-X-18
-                      <span class="pull-right">SF</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-018
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-red">
-                    <div class="info-box-text">
-                      1-1-X-19
-                      <span class="pull-right">SF</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-019
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-red">
-                    <div class="info-box-text">
-                      1-1-X-20
-                      <span class="pull-right">SF</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-020
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-red">
-                    <div class="info-box-text">
-                      1-1-X-21
-                      <span class="pull-right">SF</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-021
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-red">
-                    <div class="info-box-text">
-                      1-1-X-22
-                      <span class="pull-right">SF</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-022
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-red">
-                    <div class="info-box-text">
-                      1-1-X-23
-                      <span class="pull-right">SF</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-023
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-red">
-                    <div class="info-box-text">
-                      1-1-X-24
-                      <span class="pull-right">SF</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-024
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
-                  <div class="info-box bg-red">
-                    <div class="info-box-text">
-                      1-1-X-25
-                      <span class="pull-right">SF</span>
-                    </div>
-                    <div class="info-box-text">
-                      1-025
-                      <span class="pull-right">CP</span>
-                    </div>
-                    <div class="info-box-text text-center">
-                      -
-                    </div>
-                  </div>
                 </div>
               </div>
-            </div>
-            <div class="tab-pane" id="nodex2">
-              <h1>Node 2</h1>
-            </div>
-            <div class="tab-pane" id="nodex3">
-              <h1>Node 3</h1>
-            </div>
-            <div class="tab-pane" id="nodex4">
-              <h1>Node 4</h1>
-            </div>
-            <div class="tab-pane" id="nodex5">
-              <h1>Node 5</h1>
-            </div>
-            <div class="tab-pane" id="nodex6">
-              <h1>Node 6</h1>
-            </div>
-            <div class="tab-pane" id="nodex7">
-              <h1>Node 7</h1>
-            </div>
-            <div class="tab-pane" id="nodex8">
-              <h1>Node 8</h1>
-            </div>
-            <div class="tab-pane" id="nodex9">
-              <h1>Node 9</h1>
-            </div>
-            <div class="tab-pane" id="nodex10">
-              <h1>Node 10</h1>
             </div>
           </div>
         </div>
@@ -413,7 +61,7 @@
           <ul id="node-y-tabs" class="nav nav-tabs node-tabs">
             <!-- Node tabs for Y side created dynamically -->
           </ul>
-          <div class="tab-content mio-tabs">
+          <div id="mio-y-table" class="tab-content mio-tabs">
             <div class="tab-pane active" id="nodey1">
               <div class="container-fluid">
                 <div class="row">
@@ -766,33 +414,6 @@
                 </div>
               </div>
             </div>
-            <div class="tab-pane" id="nodey2">
-              <h1>Node 2</h1>
-            </div>
-            <div class="tab-pane" id="nodey3">
-              <h1>Node 3</h1>
-            </div>
-            <div class="tab-pane" id="nodey4">
-              <h1>Node 4</h1>
-            </div>
-            <div class="tab-pane" id="nodey5">
-              <h1>Node 5</h1>
-            </div>
-            <div class="tab-pane" id="nodey6">
-              <h1>Node 6</h1>
-            </div>
-            <div class="tab-pane" id="nodey7">
-              <h1>Node 7</h1>
-            </div>
-            <div class="tab-pane" id="nodey8">
-              <h1>Node 8</h1>
-            </div>
-            <div class="tab-pane" id="nodey9">
-              <h1>Node 9</h1>
-            </div>
-            <div class="tab-pane" id="nodey10">
-              <h1>Node 10</h1>
-            </div>
           </div>
         </div>
       </div>
@@ -806,20 +427,38 @@
     nodeInfo.forEach(function(node) {
       if ($('#node-x-table .node-tab[node_id="'+node.node+'"]').length === 0) {
         let nodeXTab = createNodeTabs(node, 'x');
-        $('#node-x-table .node-tabs').append(nodeXTab);
+        $('#node-x-tabs').append(nodeXTab);
       }
 
       if ($('#node-y-table .node-tab[node_id="'+node.node+'"]').length === 0) {
         let nodeYTab = createNodeTabs(node, 'y');
-        $('#node-y-table .node-tabs').append(nodeYTab);
+        $('#node-y-tabs').append(nodeYTab);
       }
     });
 
-    if ($('#node-x-table .node-tabs').children().length > 0) {
+    if ($('#node-x-tabs').children().length > 0) {
       $('#node-x-table .node-tab[node_id="1"]').addClass('active');
     }
-    if ($('#node-y-table .node-tabs').children().length > 0) {
+    if ($('#node-y-tabs').children().length > 0) {
       $('#node-y-table .node-tab[node_id="1"]').addClass('active');
+    }
+
+    for (let i = 0; i < 25; i++) {
+      let portBox = '<div class="info-box bg-gray-active disabled">' +
+                      '<div class="info-box-text">' +
+                        '-' +
+                        '<span class="pull-right">-</span>' +
+                      '</div>' +
+                      '<div class="info-box-text">' +
+                        '-' +
+                        '<span class="pull-right">-</span>' +
+                      '</div>' +
+                      '<div class="info-box-text text-center">' +
+                        '-' +
+                      '</div>'
+                    '</div>';
+
+      $('#x-port-grid').append(portBox);
     }
   }
 
