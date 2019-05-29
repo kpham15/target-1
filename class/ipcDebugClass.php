4e5711b3bb55849e67a7822a55012ee6c9226890
Alexander Nguyen-Huynh <AlexNHPQ@ALEXANDERs-MacBook-Pro.local>
2019-05-29 18:12:11 -0500

    modified the way debug logs

--- class/ipcDebugClass.php
+++ class/ipcDebugClass.php
@@ -1,48 +1,49 @@
-<?php
+    <?php
 
-class DEBUG{
-    public $enable = '0';
-    public $debugFile = null;
-    public $rslt;
-    public $reason;
+    class DEBUG{
+        public $enable = '0';
+        public $debugFile = null;
+        public $rslt;
+        public $reason;
 
-    public function __construct($mode=NULL){
+        public function __construct($mode=NULL){
 
-        $str = file_get_contents("../../ipc-debug.cfg");
-        if ($str == '0' || $str == '1') {
-            $this->enable = $str;
-        }
-    
-        if ($this->enable == '1') {
-            $this->debugFile =  fopen("../report/debug.log", "a");
-        }
+            $str = file_get_contents("../../ipc-debug.cfg");
+            if ($str == '0' || $str == '1') {
+                $this->enable = $str;
+            }
         
-        $this->rslt = 'success';
-        $this->reason = "DEBUG CONSTRUCTED";
+            if ($this->enable == '1') {
+                $this->debugFile =  fopen("../report/debug.log", "a");
+                $this->log("-----------------------\n".date("Y-m-d H:i:s"));
+            }
+            
+            $this->rslt = 'success';
+            $this->reason = "DEBUG CONSTRUCTED";
 
-    }
-
-    public function close() {
-        if ($this->enable == 0)
-            return;
-        fclose($this->debugFile);
-    }
-
-    public function log($string) {
-        if ($this->enable == 0) {
-            return;
         }
 
-        fwrite($this->debugFile, $string);
+        public function close() {
+            if ($this->enable == 0)
+                return;
+            fclose($this->debugFile);
+        }
+
+        public function log($string) {
+            if ($this->enable == 0) {
+                return;
+            }
+
+            fwrite($this->debugFile, "-----------------------\n" . $string);
+
+        }
+
+        
 
     }
 
-    
-
-}
 
 
 
 
-
-?>
+    ?>
