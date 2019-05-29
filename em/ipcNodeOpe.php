d15590be72b28bc95a83be5bdf60251c6ca64789
Kris Pham <kristopher.pham@gmail.com>
2019-05-29 10:30:41 -0500

    added comments about alm create

--- em/ipcNodeOpe.php
+++ em/ipcNodeOpe.php
@@ -654,6 +654,7 @@ function exec_resp($node, $hwRsp, $userObj) {
     if ($cpsObj->serial_no !== $serial_no) {
         // create alarm here almid=node-cps-sn
         $almid = "$node-cps-$serial_no";
+        // check if alm with this almid already exists
         $almObj = new ALMS($almid);
         if (count($almObj->rows) == 0) {
             $src = 'EQUIP';
