3b0ec60e9c5849821972f738c0f6e6d3e869ce7f
Bien Nguyen <bien.g.nguyen@gmail.com>
2019-05-30 18:25:51 -0500

    query msg owners

--- pages/brdcst/brdcst.php
+++ pages/brdcst/brdcst.php
@@ -13,6 +13,47 @@
 </div>
 
 <script type="text/javascript">
+  function loadOwner() {
+    $.ajax({
+      type: 'POST',
+      url: ipcDispatch,
+      data: {
+        "api": "ipcUser",
+        "act": "queryBrdcstOwner",
+        "user": user.uname
+      },
+      dataType: 'json'
+    }).done(function(data) {
+      let res = data.rows;
+      let modal = {
+        title: data.rslt,
+        body: data.reason
+      }
+
+      if (data.rslt === 'fail') {
+        modal.type = "danger",
+        modalHandler(modal);
+      } else {
+        createMsgOwners(res);
+      }
+    });
+  }
+
+  function createMsgOwners(data) {
+    var a = [];
+    a.push('<option value=""></option>');
+    
+    data.forEach(function(owner) {
+      let html =  '<option value="'+owner.uname+'">' +
+                    owner.fname + ' ' + owner.mi + ' ' + owner.lname +
+                  '</option>';
+
+      a.push(html);
+    });
+
+    $('#brdcst-modal-owner').html(a.join(''));
+  }
+  
   function clearErrors() {
     $('.help-block').text('');
     $('.form-group').removeClass('has-error');
