540af7588ff58f6abbdafaa50008d90f11a10d3a
Bien Nguyen <bien.g.nguyen@gmail.com>
2019-05-30 15:30:27 -0500

    test columns

--- pages/brdcst/brdcst.php
+++ pages/brdcst/brdcst.php
@@ -1,3 +1,10 @@
 <div id="brdcst-page" class="content-page active-page" style="display:none;">
-  <h1>Hello IPC!</h1>
+  <div class="container-fluid">
+    <div class="col-md-6">
+      <h1>Column 1</h1>
+    </div>
+    <div class="col-md-6">
+      <h1>Columnb 2</h1>
+    </div>
+  </div>
 </div>
