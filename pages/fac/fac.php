316ad175c954a1254c14559c1b037b38fb054997
Alexander Nguyen-Huynh <AlexNHPQ@ALEXANDERs-MacBook-Pro.local>
2019-05-31 11:06:25 -0500

    added fac.php

---
+++ pages/fac/fac.php
@@ -0,0 +1,16 @@
+<div id="fac-page" class="content-page" style="display:none;">
+  <div class="container-fluid">
+    
+     <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          Setup Facilities
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+          <li class="active">Setup Facilities</li>
+        </ol>
+      </section>
+
+  </div>
+</div>
