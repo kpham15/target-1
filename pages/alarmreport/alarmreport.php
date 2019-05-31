ed4a3ea008d3af0e810609764deb23dc61b5bc26
Kris Pham <kristopher.pham@gmail.com>
2019-05-31 18:10:22 -0500

    make new files and route them. display separation

--- pages/alarmreport/alarmreport.php
+++ pages/alarmreport/alarmreport.php
@@ -11,5 +11,12 @@
           <li class="active">Alarm Report</li>
         </ol>
       </section>
+
+      <!-- Alarm Report Forms -->
+      <?php include __DIR__ . '/alarmreport-forms.html'; ?>
+      
+      <!-- Alarm Report Table -->
+      <?php include __DIR__ . '/alarmreport-table.html'; ?>
+      
   </div>
 </div>
