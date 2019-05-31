0420bf9f3b5ad0bda6a646cc9045442bdc7bcb82
Kris Pham <kristopher.pham@gmail.com>
2019-05-31 10:49:00 -0500

    create references dir and php

---
+++ pages/references/references.php
@@ -0,0 +1,15 @@
+<div id="references-page" class="content-page" style="display:none;">
+  <div class="container-fluid">
+    
+     <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          References
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+          <li class="active">References</li>
+        </ol>
+      </section>
+  </div>
+</div>
