7695ea08b80a0a942af0ef9c7fff4dad70ca4816
Bien Nguyen <bien.g.nguyen@gmail.com>
2019-05-31 12:42:36 -0500

    error validation function

--- js/functions.php
+++ js/functions.php
@@ -55,7 +55,8 @@
 	}
 
 	function inputError(selector, string) {
-		selector.siblings('.help-block').text(string);
+		let helpBlock = '<span class="help-block">'+string+'</span>';
+		selector.parent('.form-group').append(helpBlock);
 		selector.parent('.form-group').addClass('has-error');
 		return;
 	}
