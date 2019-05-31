53df7ee3ed87036164a4f105fa872943d3477fed
Bien Nguyen <bien.g.nguyen@gmail.com>
2019-05-31 17:19:16 -0500

    move modals

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
 	//IPCv2
 	include './pages/nav-wrapper/nav-wrapper.php';
 	include './pages/login.html';
-	include './pages/modals.html';
+	include './pages/modals.php';
 	
 	include './js/functions.php';
 	include './js/variables.php';
