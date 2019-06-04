34cadb92e47a4620ea76246da03fb541837cbf66
Kris Pham <kristopher.pham@gmail.com>
2019-06-04 18:50:59 -0500

    create all related files and routed with click on side bar

---
+++ pages/alarm-admin/alarm-admin.php
@@ -0,0 +1,21 @@
+<div id="alarm-admin-page" class="content-page" style="display:none;">
+  <div class="container-fluid">
+    
+     <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          Alarm Administration
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+          <li class="active">Alarm Administration</li>
+        </ol>
+      </section>
+      <!-- Node Operation Forms -->
+      <?php include __DIR__ . '/alarm-admin-forms.html'; ?>
+
+      <!-- Node Operation Table -->
+      <?php include __DIR__ . '/alarm-admin-table.html'; ?>
+      
+  </div>
+</div>
