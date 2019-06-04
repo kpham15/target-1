3629b9e65b7b6377d47e70ab878e09de61f6f1ff
bhdninh1 <you@example.com>
2019-06-04 16:28:41 -0500

    correct debug-class

--- em/ipcNodeOpe.php
+++ em/ipcNodeOpe.php
@@ -461,10 +461,11 @@ function updateCpsStatus($hwRsp) {
 // function called by updateAlm in case string contains voltage only
 // str looks like this "$ackid=1-cps-csta,status,voltage1=46587mV,voltage2=47982mV,voltage3=48765mV,voltage4=49234mV,backplane=IAMAMIOXUUIDTHATYOUCANTDECODE*"
 function updateCpsVolt($hwRsp) {
-    global $debugObj;
+    
+    $debugObj = new DEBUG();
 
     $debugObj->log($hwRsp);
-    
+
     // filters data brought from $hwRsp and extracts voltage values
     $newCmd = substr($hwRsp, 1, -1);
     $splitCmd = explode(',', $newCmd);
