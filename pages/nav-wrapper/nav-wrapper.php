6a01c474e8c7f8dc08970b1f1fd63c1ef9ef6fce
Kris Pham <kristopher.pham@gmail.com>
2019-06-05 05:08:15 +0000

    Resolve "Add alarm-report-page to ipcV2"

--- pages/nav-wrapper/nav-wrapper.php
+++ pages/nav-wrapper/nav-wrapper.php
@@ -16,6 +16,7 @@
     
     <!-- Includes for all the content pages -->
     <?php include __DIR__ . '/../system-view/system-view.php'; ?>
+    <?php include __DIR__ . '/../alarmreport/alarmreport.php'; ?>
     <?php include __DIR__ . '/../brdcst/brdcst.php'; ?>
     <?php include __DIR__ . '/../references/references.php'; ?>
     <?php include __DIR__ . '/../svc/svc.php'; ?>
