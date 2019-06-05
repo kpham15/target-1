a634cef68d01792026577a82c415563effdf2282
Thanh Pham <thanh.pham@bhd-solutions.com>
2019-06-05 22:39:54 +0000

    modify svc and prov-report

--- pages/portmap/portmap.php
+++
@@ -1,9 +0,0 @@
-<div id="portmap-page" class="content-page" style="display:none">
-    <div class="container-fluid">
-        <section class="contend-header" style="">
-
-        </section>
-
-    </div>
-
-</div>
