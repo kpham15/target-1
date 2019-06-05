60202fbdf5657118b3fbf79fa815060d163723d1
thanhpv8 <thanhpv678@gmail.com>
2019-06-04 23:12:51 -0500

    doing provisiong report

--- pages/portmap/portmap.php
+++ pages/portmap/portmap.php
@@ -1,8 +1,15 @@
 <div id="portmap-page" class="content-page" style="display:none">
     <div class="container-fluid">
-        <section class="contend-header" style="">
-
+        <section class="contend-header" style="padding:2px">
+            <h1>SETUP PORT MAPPING</h1>
+            <ol>
+                <li><a href="#"><i class="fa fa-dashboard"></i>Home</a></li>
+                <li>Setup Port Mapping</li>
+            </ol>
         </section>
+        <?php include __DIR__."/portmap/portmap-form.html" ?>
+        <?php include __DIR__."/portmap/portmap-table.html" ?>
+        <?php include __DIR__."/portmap/portmap-modal.html" ?>
 
     </div>
 
