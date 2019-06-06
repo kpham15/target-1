e73fac880498c092809925699dbb7c428a9c22be
Kris Pham <kristopher.pham@gmail.com>
2019-06-06 17:50:35 -0500

    created directory and routed backup page

---
+++ pages/database-backup/database-backup.php
@@ -0,0 +1,19 @@
+<div id="database-backup-page" class="content-page" style="display:none;">
+  <div class="container-fluid">
+    
+     <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          Database Backup
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+          <li class="active">Database Backup</li>
+        </ol>
+      </section>
+
+      <!-- Database Backup Page -->
+      <?php include __DIR__ . '/database-backup-forms.html'; ?>
+     
+  </div>
+</div>
