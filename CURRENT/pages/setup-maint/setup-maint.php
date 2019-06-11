7378624fb5c6be3a9be03667496fe9ab4bad9205
Kris Pham <kristopher.pham@gmail.com>
2019-06-11 16:50:15 -0500

    loadfacX function

--- CURRENT/pages/setup-maint/setup-maint.php
+++ CURRENT/pages/setup-maint/setup-maint.php
@@ -23,3 +23,39 @@
 
   </div>
 </div>
+
+<script type="text/javascript">
+  function maintLoadFacX() {
+    $.ajax({
+      type:       'POST',
+      url:        ipcDispatch,
+      data:       {
+        "api":    "ipcProv",
+        "act":    "queryFacX",
+        "user":   user.uname,
+      },
+      dataType:   'json'
+    }).done(function(data) {
+      let res = data.rows;
+      let modal = {
+        header:   data.rslt,
+        body:     data.reason,
+      }
+
+      if (data.rslt == 'fail') {
+        modal.type = 'danger';
+        modalHandler(modal);
+      }
+      else {
+        let a = [];
+        a.push('<option></option');
+
+        for (let i=0; i<res.length; i++)m {
+          a.push('<option>' + data.rows[i].fac + '</option>');
+        }
+        $('#setup-maint-modal-conn2-ffac').empty();
+        $('#setup-maint-modal-conn2-ffac').html(a.join(""));
+      }
+    })
+  }
+</script>
