e749c237233025e35a92a87ee739095850e77afe
ALEXANDER NGUYEN-HUYNH <alex.nguyen@bhd-solutions.com>
2019-06-11 15:59:58 -0500

    added form

--- CURRENT/pages/matrix/matrix.php
+++ CURRENT/pages/matrix/matrix.php
@@ -4,7 +4,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        LOCK/UNLOCK MATRIX CARDS AND NODES
+        Lock/Unlock Matrix Cards and Nodes
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
