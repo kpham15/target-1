247fb4535e97648f377745838203a2311bc65219
Kris Pham <kristopher.pham@gmail.com>
2019-06-11 16:58:33 -0500

    loadfacy function

--- CURRENT/pages/setup-maint/setup-maint.php
+++ CURRENT/pages/setup-maint/setup-maint.php
@@ -25,6 +25,43 @@
 </div>
 
 <script type="text/javascript">
+  function maintLoadFacY() {
+    $.ajax({
+      type:     'POST',
+      url:      ipcDispatch,
+      data:     {
+        "api":  "ipcProv",
+        "act":   "queryFacY",
+        "user":  user.uname,
+      },
+      dataType:  'json'
+    }).done(function(data) {
+      let res = data.rows;
+      let modal = {
+        header: data.rslt,
+        body:   data.reason,
+      }
+
+      if (data.rslt == 'fail') {
+        modal.type = 'danger';
+        modalHandler(modal);
+      }
+      else {
+        let a = [];
+        a.push('<option value=""></option>');
+
+        for (let i=0; i<res.length; i++) {
+          a.push('<option>' + data.rows[i].fac + '</option>');
+        }
+
+        $('#setup-maint-modal-conn1-tfac').empty();
+        $('#setup-maint-modal-conn1-tfac').html(a.join(""));
+
+
+      }
+    })
+  }
+
   function maintLoadFacX() {
     $.ajax({
       type:       'POST',
@@ -48,12 +85,12 @@
       }
       else {
         let a = [];
-        a.push('<option></option');
+        a.push('<option value=""></option>');
 
         for (let i=0; i<res.length; i++) {
           a.push('<option>' + data.rows[i].fac + '</option>');
         }
-        
+
         $('#setup-maint-modal-conn2-ffac').empty();
         $('#setup-maint-modal-conn2-ffac').html(a.join(""));
       }
