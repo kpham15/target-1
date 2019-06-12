4fb1de52690a56f426695bef73d46282d6fed4aa
Dat Vu <datvu22@gmail.com>
2019-06-12 13:44:32 -0500

    help search modal

--- pages/help-search/help-search.php
+++ pages/help-search/help-search.php
@@ -13,6 +13,7 @@
     <!--setup help forms-->
     <?php include __DIR__ . '/help-search-forms.html'; ?>
     <?php include __DIR__ . '/help-search-table.html'; ?>
+    <?php include __DIR__ . '/help-search-modal.html'; ?>
   </div>
 
 </div>
