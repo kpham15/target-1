422ea70b4c5e03ee70d7e3b825db625983d7b5a4
Kris Pham <kristopher.pham@gmail.com>
2019-06-13 16:25:50 -0500

    change title of tables, add click view action to hide cons table

--- CURRENT/pages/setup-maint/setup-maint.php
+++ CURRENT/pages/setup-maint/setup-maint.php
@@ -8,7 +8,7 @@
         </h1>
         <ol class="breadcrumb">
           <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-          <li class="active">Setup Maintenance</li>
+          <li class="active">Setup Maintenance Connection</li>
         </ol>
       </section>
 
