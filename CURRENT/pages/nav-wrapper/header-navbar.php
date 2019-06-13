b8a320f52e63dfc6e7995e45b96c7c04a899b9f6
Bien Nguyen <bien.g.nguyen@gmail.com>
2019-06-13 17:24:24 -0500

    fix path

--- CURRENT/pages/nav-wrapper/header-navbar.php
+++ CURRENT/pages/nav-wrapper/header-navbar.php
@@ -108,7 +108,7 @@
   </nav>
 </header>
 
-<?php include __DIR__ . "/../header-nav-modals.html"; ?>
+<?php include __DIR__ . "/header-nav-modals.html"; ?>
 
 <script type="text/javascript">
   function updateUsername() {
