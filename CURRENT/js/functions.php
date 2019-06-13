25d26e7e87612b85aae53ae58ee0df6980507820
Thanh Pham <thanh.pham@bhd-solutions.com>
2019-06-13 17:43:24 +0000

    finished  home-page-add-find-facility-button into ipcv2

--- CURRENT/js/functions.php
+++ CURRENT/js/functions.php
@@ -57,7 +57,7 @@
 	function loginSuccess() {
 		$('#login-page').hide();
 		$('#nav-wrapper').show();
-
+		
 		getSystemInfo();
 	}
 
@@ -184,4 +184,40 @@
 
         return result;
 	}
+
+
+	// ================ Encode Password ================= //
+	var keyId = "";
+	function encode(data) {
+		var header = {
+			"alg": "HS256",
+			"typ": "JWT"
+		};
+	
+		var stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header));
+		var encodedHeader = base64url(stringifiedHeader);
+	
+		var stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(data));
+		var encodedData = base64url(stringifiedData);
+	
+		var signature = encodedHeader + "." + encodedData;
+		signature = CryptoJS.HmacSHA256(signature, keyId);
+		signature = base64url(signature);
+		return encodedHeader + "." + encodedData + "." + signature;
+    }
+  
+    function base64url(source) {
+		// Encode in classical base64
+		encodedSource = CryptoJS.enc.Base64.stringify(source);
+	
+		// Remove padding equal characters
+		encodedSource = encodedSource.replace(/=+$/, '');
+		
+		// Replace characters according to base64url specifications
+		encodedSource = encodedSource.replace(/\+/g, '-');
+		encodedSource = encodedSource.replace(/\//g, '_');
+	
+		return encodedSource;
+    }
+  
 </script>
