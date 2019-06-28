27a33c05dd6e036493b5d14aad6451b7b6c63fe5
Kris Pham <kristopher.pham@gmail.com>
2019-06-28 10:39:00 -0500

    change php file, header and breadcrumb padding

--- CURRENT/pages/ftOrders/ftOrders.php
+++ CURRENT/pages/ftOrders/ftOrders.php
@@ -2,11 +2,11 @@
   <div class="container-fluid">
     
     <!-- Content Header (Page header) -->
-    <section class="content-header">
+    <section class="content-header" style="padding:2px;">
       <h1>
         FLOW-THROUGH ORDERS
       </h1>
-      <ol class="breadcrumb">
+      <ol class="breadcrumb" style="padding-top: 0px">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
         <li class="active">Flow-Through Orders</li>
       </ol>
