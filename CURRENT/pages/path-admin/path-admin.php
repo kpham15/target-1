c39bb136d9749c51d41c0346b16f4c39776fafb1
Kris Pham <kristopher.pham@gmail.com>
2019-07-01 14:58:32 -0500

    uppercase header

--- CURRENT/pages/path-admin/path-admin.php
+++ CURRENT/pages/path-admin/path-admin.php
@@ -3,7 +3,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header" style="padding:2px;">
       <h1>
-        Path Administration
+        PATH ADMINISTRATION
       </h1>
       <ol class="breadcrumb" style="padding-top: 0px">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
