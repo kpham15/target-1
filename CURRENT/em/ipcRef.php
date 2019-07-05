a5d2c768f2d021e1e5e6bb38aa43d9387bbd2c48
Kris Pham <kristopher.pham@gmail.com>
2019-07-05 16:57:42 -0500

    testing

--- CURRENT/em/ipcRef.php
+++ CURRENT/em/ipcRef.php
@@ -175,7 +175,7 @@ function updateRefs($userObj,$refObj, $pw_expire, $pw_alert, $pw_reuse, $pw_repe
         return $result;
     }
 
-    $result['reason'] = "PW EXPIRE VALUE IS ";
+    $result['reason'] = "PW EXPIRE VALUE IS " . $refObj->ref['volt_range'];
     return $result;
     // if ($pw_expire != $refObj->ref['pw_expire'])
 
