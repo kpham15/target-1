167070eeb22a447c4042d07d538496bc0976e687
ALEXANDER NGUYEN-HUYNH <alex.nguyen@bhd-solutions.com>
2019-07-05 18:09:56 -0500

    t

--- CURRENT/em/ipcLogin.php
+++ CURRENT/em/ipcLogin.php
@@ -91,7 +91,7 @@
 
         $pw4Date = strtotime($userObj->t4);
         $pw4Age = ceil(($now - $pw4Date) / (60 * 60 * 24));
-        echo $userObj->pwdate;
+        echo $userObj->pw;
 
         if ($refObj->ref['pw_reuse'] == "0") {
             return true;
