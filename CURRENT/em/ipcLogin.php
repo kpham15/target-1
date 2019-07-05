60c197f65b27c5d387852e734457449e95773160
ALEXANDER NGUYEN-HUYNH <alex.nguyen@bhd-solutions.com>
2019-07-05 17:15:51 -0500

    test

--- CURRENT/em/ipcLogin.php
+++ CURRENT/em/ipcLogin.php
@@ -354,7 +354,7 @@
                     } else {
                         $result['rslt'] = FAIL;
                         // $result["reason"] = "REUSE OF LAST " . $refObj->ref['pw_reuse'] . " PASSWORD IS NOT ALLOWED";
-                        $result["reason"] = $now . " : " . $pwDate;
+                        $result["reason"] = $pwAge;
                         return $result;
                     }
                 }
@@ -365,7 +365,7 @@
                     } else {
                         $result['rslt'] = FAIL;
                         // $result["reason"] = "REUSE OF LAST " . $refObj->ref['pw_reuse'] . " PASSWORD IS NOT ALLOWED";
-                        $result["reason"] = $now . " : " . $pwDate;
+                        $result["reason"] = $pwAge;
                         return $result;
                     }
                 }
