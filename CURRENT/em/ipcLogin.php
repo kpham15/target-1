a8549f31971c8ceecfa2d66a22b8ffdb4c5f6b42
ALEXANDER NGUYEN-HUYNH <alex.nguyen@bhd-solutions.com>
2019-07-05 17:17:04 -0500

    test

--- CURRENT/em/ipcLogin.php
+++ CURRENT/em/ipcLogin.php
@@ -75,22 +75,22 @@
         $userObj = new USERS($user);
         $now = time();
         $pwDate = strtotime($userObj->pwdate);
-        $pwAge = round(($now - $pwDate) / (60 * 60 * 24));
+        $pwAge = ceil(($now - $pwDate) / (60 * 60 * 24));
 
         $pw0Date = strtotime($userObj->pw0);
-        $pw0Age = round(($now - $pw0Date) / (60 * 60 * 24));
+        $pw0Age = ceil(($now - $pw0Date) / (60 * 60 * 24));
 
         $pw1Date = strtotime($userObj->pw1);
-        $pw1Age = round(($now - $pw1Date) / (60 * 60 * 24));
+        $pw1Age = ceil(($now - $pw1Date) / (60 * 60 * 24));
 
         $pw2Date = strtotime($userObj->pw2);
-        $pw2Age = round(($now - $pw2Date) / (60 * 60 * 24));
+        $pw2Age = ceil(($now - $pw2Date) / (60 * 60 * 24));
 
         $pw3Date = strtotime($userObj->pw3);
-        $pw3Age = round(($now - $pw3Date) / (60 * 60 * 24));
+        $pw3Age = ceil(($now - $pw3Date) / (60 * 60 * 24));
 
         $pw4Date = strtotime($userObj->pw4);
-        $pw4Age = round(($now - $pw4Date) / (60 * 60 * 24));
+        $pw4Age = ceil(($now - $pw4Date) / (60 * 60 * 24));
 
         if ($refObj->ref['pw_reuse'] == "0") {
             return true;
@@ -353,8 +353,8 @@
                         $userObj->updatePw_firstTime($newPw);
                     } else {
                         $result['rslt'] = FAIL;
-                        // $result["reason"] = "REUSE OF LAST " . $refObj->ref['pw_reuse'] . " PASSWORD IS NOT ALLOWED";
-                        $result["reason"] = $pwAge;
+                        $result["reason"] = "REUSE OF LAST " . $refObj->ref['pw_reuse'] . " PASSWORD IS NOT ALLOWED";
+                        // $result["reason"] = $pwAge;
                         return $result;
                     }
                 }
@@ -364,8 +364,8 @@
                         $userObj->updatePw($newPw);
                     } else {
                         $result['rslt'] = FAIL;
-                        // $result["reason"] = "REUSE OF LAST " . $refObj->ref['pw_reuse'] . " PASSWORD IS NOT ALLOWED";
-                        $result["reason"] = $pwAge;
+                        $result["reason"] = "REUSE OF LAST " . $refObj->ref['pw_reuse'] . " PASSWORD IS NOT ALLOWED";
+                        // $result["reason"] = $pwAge;
                         return $result;
                     }
                 }
