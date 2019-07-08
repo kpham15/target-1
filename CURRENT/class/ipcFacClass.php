14912f6e1bc3c2a8ea4bacd396e9cda42be96dbe
Kris Pham <kristopher.pham@gmail.com>
2019-07-08 13:32:45 -0500

    debug

--- CURRENT/class/ipcFacClass.php
+++ CURRENT/class/ipcFacClass.php
@@ -263,8 +263,7 @@
                 // check dash in beginning and end
                 if ($facBeginLetter == '-' || $facLastLetter == '-') {
                     $this->rslt = FAIL;
-                    // $this->reason = "INVALID FAC FORMAT";
-                    $this->reason = "dash front back";
+                    $this->reason = "INVALID FAC FORMAT";
                     return false;
                 }
 
